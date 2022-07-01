c498f9cca166f956465544c4600c4d774031cdb7
Allie Dunkel <alliedunkel@outlook.com>
2022-06-30 20:35:33 -0500

    added a from

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+"use strict";
+
 // let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm',
 //   '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
@@ -330,4 +332,5 @@ function footerHourlyStoreTotals(){
   
   
   StoreLocation.renderAll();
+
 
